39b0f8b3c5e586eea2f6532474d173115d489032
DanMarzo <marzogildan@gmail.com>
2022-12-25 18:57:30 -0300

    Inclusão de Connection string na Class Program

--- LanchesMac/Models/Lanche.cs
+++ LanchesMac/Models/Lanche.cs
@@ -1,8 +1,20 @@
-﻿namespace LanchesMac.Models;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models;
+[Table("Laches")]
 public class Lanche
 {
+    [Key]
     public int LancheId { get; set; }
+    [Required(ErrorMessage ="O nome do lanche é obrigatório")]//o error mensage é para evitar da msg saia no idioma ingles
+    [Display(Name ="Nome do lanche")]
     public string Nome { get; set; }
+    [Required(ErrorMessage = "A Descrição Curta é obrigatória")]
+    [Display(Name = "Nome do lanche")]
+    [MinLength(2, ErrorMessage = "Não pode ser menor que 2 caracteres")]
+    [MaxLength(100, ErrorMessage = "Não pode ser maior que 100 caracteres")]
     public string DescricaoCurta { get; set; }
     public string DescricaoDetalhada { get; set; }
     public decimal Preco { get; set; }
