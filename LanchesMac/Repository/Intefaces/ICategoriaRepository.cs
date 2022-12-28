46ed5b1cc9ea1870968ac8c8a3610bb5a372c07e
DanMarzo <marzogildan@gmail.com>
2022-12-27 22:52:28 -0300

    Criando Padrão Repository - Pesquisar mais Interfaces
    ICategoriaRepository retorna um IEnumerable somente de leitura

---
+++ LanchesMac/Repository/Intefaces/ICategoriaRepository.cs
@@ -0,0 +1,9 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repository.Intefaces
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
