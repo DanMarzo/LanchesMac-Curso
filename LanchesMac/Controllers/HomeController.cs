41c4cd73656dc4fcb9d21d8458bca183c45e31fe
DanMarzo <marzogildan@gmail.com>
2023-01-03 15:31:03 -0300

    ViewData, ViewBag, TempData
    Realizando aplicações e teste com cada

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        TempData["NomeTeste"] = "Thiagão";
         return View();
     }
     public IActionResult Demo()
